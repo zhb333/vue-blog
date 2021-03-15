aa0c128d2f394f80ddc8541e0d7fe5367b6e0360
zhb333 <1140457303@qq.com>
2021-03-16 00:10:29 +0800

    引入 element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App)
+const app = createApp(App)
+
+app
   .use(store)
   .use(router)
-  .mount("#app");
+  .use(ElementPlus)
+  .mount('#app')
